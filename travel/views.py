9ee0e273d210134a3b2d1abf3579957d74f4dd6f
Kochevnik <sam.kochevnik.uz@gmail.com>
2022-10-01 11:53:27 +0500

    first commit

--- travel/views.py
+++ travel/views.py
@@ -6,4 +6,9 @@ from django.shortcuts import render
 def home(request):
     name = 'Bob'
 
-    return render(request, 'home.html', {'name':name})
+    return render(request, 'home.html', {'name': name})
+
+def about(request):
+    name = 'About us'
+
+    return render(request, 'about.html', {'name': name})
